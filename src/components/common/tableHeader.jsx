a6a8cdc9c3c6b58065584ba219af3c677aba5e58
Lenin Paulino <leninpaulinon@gmail.com>
2019-09-06 15:06:50 -0400

    Add sort icon to table header

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -11,6 +11,16 @@ class TableHeader extends Component {
     }
     this.props.onSort(sortColumn);
   };
+
+  renderSortIcon = column => {
+    const { sortColumn } = this.props;
+    if (!column.path || column.path !== sortColumn.path) return null;
+
+    if (sortColumn.order === "asc") return <i className="fa fa-sort-asc" />;
+
+    return <i className="fa fa-sort-desc" />;
+  };
+
   render() {
     return (
       <thead>
@@ -20,7 +30,7 @@ class TableHeader extends Component {
               key={column.path || column.key}
               onClick={() => this.raiseSort(column.path)}
             >
-              {column.label}
+              {column.label} {this.renderSortIcon(column)}
             </th>
           ))}
         </tr>
